d7ebaee9075ed5ad140bee119a449bcd946879ad
Henry <lihenryhl@yahoo.com>
2022-04-11 03:18:44 -0700

    connected front and back end points and worked with axios

--- client/gui/src/App.js
+++ client/gui/src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
-import {useState} from 'react'
+import  {Signup} from './components/Signup';
+import { Display } from './components/display';
 
 function App() {
-  const [newUser,setnewUser] = useState({
-    username:"",
-    password:""
-  })
-
-  const handleForm = (e) =>{
-    const {name,value} = e.target
-    e.preventDefault()
-    setnewUser({...newUser, [name]:value});
-    console.log(newUser)
-  }
 
   return (
     <div className="App">
-      <form onSubmit={handleForm}>
-        <label>username</label>
-        <input name = "username" value = {newUser.username} onChange={handleForm}></input>
-        <label>password</label>
-        <input name = "password" value = {newUser.password} onChange={handleForm}></input>
-        <button type='submit'>submit</button>
-      </form>
+      <Signup/>
+      <Display/>
     </div>
   );
 }
