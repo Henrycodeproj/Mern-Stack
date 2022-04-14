3ebdc4bebcd2a850c215af3fd152b1b58d3cdd62
Henry <lihenryhl@yahoo.com>
2022-04-14 02:17:15 -0700

    started working on styling, logic is already finished for route.

---
+++ client/gui/src/components/navigation/navbar.jsx
@@ -0,0 +1,11 @@
+import {Button} from '@mui/material'
+export const Navbar = () =>{
+    return(
+        <nav>
+            <div className="logo">Unplug</div>
+            <ul className="list-container">
+                <Button variant="contained" color = "warning">Login</Button>
+            </ul>
+        </nav>
+    )
+}
---
+++ client/gui/src/components/signup/SideImage.jsx
@@ -0,0 +1,9 @@
+import connect from '../../images/connect.jpg'
+
+export const SideImage = () =>{
+    return (
+        <div>
+            <img src ={connect} alt ="logo"></img>
+        </div>
+    )
+}
