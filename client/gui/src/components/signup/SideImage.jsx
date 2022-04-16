6e9064e79709061c210bb55612b7071bfbf68e30
Henry <lihenryhl@yahoo.com>
2022-04-16 03:27:58 -0700

    there is a logic bug with checking requirements, needs to be looked through.

--- client/gui/src/components/signup/SideImage.jsx
+++ client/gui/src/components/signup/SideImage.jsx
@@ -1,9 +1,9 @@
-import connect from '../../images/connect.jpg'
-
+//import connect from '../../images/connect.jpg'
+import test from '../../images/test.png'
 export const SideImage = () =>{
     return (
         <div>
-            <img src ={connect} alt ="logo"></img>
+            <img src ={'    https://scontent-sjc3-1.xx.fbcdn.net/v/t1.15752-9/277848517_389876082958013_8533265528147998505_n.png?_nc_cat=101&ccb=1-5&_nc_sid=ae9488&_nc_ohc=7fVICvUF2gwAX-OV-BK&_nc_ht=scontent-sjc3-1.xx&oh=03_AVI3O40IEJGdy1wRUN_Sgi4FkmC-8PA3xVuwC15Jq6gX2A&oe=6280E68E'} alt ="logo"></img>
         </div>
     )
 }
