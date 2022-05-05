a6dc0308ee3ac0de8acbd42e67f85ae8caee50e6
Henry <lihenryhl@yahoo.com>
2022-05-05 03:17:50 -0700

    finally setup email sending through sendgrid and will now get started on sending a token and auth before working on protected routes.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 import UserModel from './Models/Users.js';
 import bcrypt from 'bcrypt'
+import nodemailer from "nodemailer"
+import sgMail from "@sendgrid/mail"
+import crypto from "crypto"
+
 
 
 const app = express()
@@ -13,6 +17,10 @@ app.use(express.json());
 dotenv.config()
 
 const password = process.env.password
+const emailusername = process.env.username
+const emailpassword = process.env.emailpassword
+
+sgMail.setApiKey(process.env.API_KEY_EMAIL)
 
 const DB_URL = `mongodb+srv://admin:${password}@cluster0.dlurz.mongodb.net/Users?retryWrites=true&w=majority`
 
@@ -49,8 +57,16 @@ app.post("/login", async (req, res)=>{
             res.status(406).send('Your account is not verified.')
         }
 
-})
-    
+})  
+const sendMail = async (msg) =>{
+    try{
+        await sgMail.send(msg)
+        console.log('email sending..')
+    } catch(error){
+        console.log(error)
+    }
+}
+
 app.post("/createUser", async (req,res) => {
     const {username,password, email} = req.body
     const newUser = new UserModel({
@@ -63,9 +79,44 @@ app.post("/createUser", async (req,res) => {
     const salt = bcrypt.genSaltSync(10)
     newUser.password = bcrypt.hashSync(password, salt)
 
-    await newUser.save().then(response =>{
+    await newUser.save() 
+    .then(response =>{     
         res.status(201).send(response)
-
+        try{ 
+        sendMail({
+            to:"lihenryhl@yahoo.com",
+            from:"hennypenny456@gmail.com",
+            subject:"HELLO",
+            text:"testing"
+            })
+        } catch(error) {
+            console.log(error)
+        }
+        //try {
+            // let transporter = nodemailer.createTransport({
+            //     service: "stmp.gmail.com",
+            //     //port: 587,
+            //     secure: false, // true for 465, false for other ports
+            //     auth: {
+            //       user: emailusername,
+            //       pass: emailpassword, 
+            //     },
+            //   });
+            
+            //   // send mail with defined transport object
+            //   let info = await transporter.sendMail({
+            //     from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            //     to: "lihenryhl@yahoo.com", // list of receivers
+            //     subject: "Hello ✔", // Subject line
+            //     text: "Hello world?", // plain text body
+            //     html: "<b>Hello world?</b>", // html body
+            //   });
+            //   console.log("Message sent: %s", info.messageId);
+            //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+            //    } catch (error){
+            //        console.log(error)
+            //    }
     }).catch (error => {
         if (error.keyValue.username && error.code === 11000){
             res.status(406).send(`This username ${error.keyValue.username} is already taken`)
