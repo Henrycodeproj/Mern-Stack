04281eced2cb4fc7a717ae152fb7430b89cf9547
Henry <lihenryhl@yahoo.com>
2022-06-20 23:28:11 -0700

    added comments and need to add usecontext for loggedin in users to help maintain sessions. Will be using react router dom v6

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -2,6 +2,7 @@ import localStrategy from 'passport-local';
 import UserModel from '../Models/Users.js';
 import bcrypt from 'bcrypt';
 
+//passport function that handles authentication with server and database
 function InitPassport(passport) {
     passport.use(new localStrategy({usernameField: 'login_username', passwordField: 'login_password'},
         function (username,password,done) {
@@ -19,12 +20,12 @@ function InitPassport(passport) {
             })
         }
     ))
-
+//creates a cookie for user
 passport.serializeUser(function(user,done) {
     console.log('serialize working')
     done(null, user.id)
 })
-
+//decrypts cookie and creates a usable function to call passport user
 passport.deserializeUser(function(id, done) {
     UserModel.findById(id, function(err, user) {
         console.log('deserializer working')
