f297a9803b6f1c16de26a264905c46c0c266bbe2
Henry <lihenryhl@yahoo.com>
2022-08-15 23:33:26 -0700

    working on profile. queries are now reversed from newest posts and working on UI portion as well as notifications

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken'
 
 function isAuthenticated(req,res,next){
-    var authorization = req.headers.authorization.split('.')[1]
+    if (!req.headers.authorization){
+        return res.send({message:"Unauthorized"})
+    }
+
     jwt.verify(req.headers.authorization, process.env.SECRET_SESSION, (err, result) =>{
         result ? req.isAuth = true: req.isAuth = false
         next()
