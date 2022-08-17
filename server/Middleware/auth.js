39f60e6f3fea5f7902960fc59b1216f9dbef1e71
Henry <lihenryhl@yahoo.com>
2022-08-17 00:15:11 -0700

    working on ui, profile, etc.

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -6,7 +6,7 @@ function isAuthenticated(req,res,next){
     }
 
     jwt.verify(req.headers.authorization, process.env.SECRET_SESSION, (err, result) =>{
-        result ? req.isAuth = true: req.isAuth = false
+        result ? req.isAuth = true : req.isAuth = false
         next()
     })
 }
