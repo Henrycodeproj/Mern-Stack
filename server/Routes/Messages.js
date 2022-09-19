0e52e59756ad01ae69aec0182394bd46d984b6dc
Henry <lihenryhl@yahoo.com>
2022-09-18 23:26:54 -0700

    trying to get correct query

--- server/Routes/Messages.js
+++ server/Routes/Messages.js
@@ -1,6 +1,7 @@
 import express from "express"
 import isAuthenticated from '../Middleware/auth.js';
 import MessageModel from "../Models/messages.js";
+import mongoose from "mongoose";
 
 export const router = express.Router()
 
@@ -8,17 +9,20 @@ router.get('/recent/all/:id', isAuthenticated, async (req, res) => {
     const currentUserId = req.params.id
     const g = await MessageModel.aggregate([
         {
-            $group : {"_id":"$name","doc":{"$first":"$$ROOT"}, lastConversations: { $addToSet: '$conversationId'}}
+            $match:{senderId:mongoose.Types.ObjectId(currentUserId)}
         },
-        {"$replaceRoot":{"newRoot":"$doc"}},
         {
             $sort: {
                 createdAt: -1
             }
         },
         {
+            $group: {_id: "$conversationId",
+            doc: {$first: "$$ROOT"}}
+        },
+        {
             $limit:3
-        }
+        },
     ])
     console.log(g)
     try {
