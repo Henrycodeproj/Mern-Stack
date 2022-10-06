24108ec899359f4af8e138bb6fdce4b135d2284b
Henry <lihenryhl@yahoo.com>
2022-10-05 23:49:51 -0700

    adding profile functionality

--- client/gui/src/components/AuthViews/ProfileViews/profile.jsx
+++ client/gui/src/components/AuthViews/ProfileViews/profile.jsx
@@ -4,14 +4,13 @@ import { useEffect, useState, useContext } from "react"
 import axios from "axios"
 import { accountContext } from "../../Contexts/appContext"
 import "./profile.css"
-import { Avatar } from "@mui/material"
+import { Avatar, Tooltip } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
-
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -20,6 +19,15 @@ import ImageIcon from '@mui/icons-material/Image';
 import WorkIcon from '@mui/icons-material/Work';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import Divider from '@mui/material/Divider';
+import EditIcon from '@mui/icons-material/Edit';
+
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import * as React from 'react';
 
 export const Profile = ()=> {
     const { user } = useContext(accountContext) 
@@ -48,6 +56,60 @@ export const Profile = ()=> {
             }
      */
 
+            const [state, setState] = React.useState({
+              top: false,
+              left: false,
+              bottom: false,
+              right: false,
+            });
+          
+            const toggleDrawer = (anchor, open) => (event) => {
+              if (
+                event &&
+                event.type === 'keydown' &&
+                (event.key === 'Tab' || event.key === 'Shift')
+              ) {
+                return;
+              }
+          
+              setState({ ...state, [anchor]: open });
+            };
+          
+            const list = (anchor) => (
+              <Box
+                sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+                role="presentation"
+                onClick={toggleDrawer(anchor, false)}
+                onKeyDown={toggleDrawer(anchor, false)}
+              >
+                <List>
+                  {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider />
+                <List>
+                  {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            );        
+
     return(
         <>
         {viewedUser &&
@@ -67,7 +129,7 @@ export const Profile = ()=> {
                             </div>
                         </div>
                         <div style = {{maxWidth:"350px", maxHeight:"350px", borderRadius:"20px", padding:"1rem"}}>
-                            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'transparent' }}>
                                 <ListItem>
                                   <ListItemAvatar>
                                     <Avatar>
@@ -96,18 +158,37 @@ export const Profile = ()=> {
                         </div>
                     </div>
                     <div>
+                        <div style ={{display:"flex", alignItems:"center", gap:"10px"}}>
                         <h1>A Little About Me...</h1>
+                          <div>
+                            {['bottom'].map((anchor) => (
+                              <React.Fragment key={anchor}>
+                                <Tooltip title = "Add/Change Self Description">
+                                  <EditIcon className = "About_Me_Edit_Button" onClick={toggleDrawer(anchor, true)} sx = {{fontSize:"1.7rem", cursor:"pointer"}}/>
+                                </Tooltip>
+                                <SwipeableDrawer
+                                  anchor={anchor}
+                                  open={state[anchor]}
+                                  onClose={toggleDrawer(anchor, false)}
+                                  onOpen={toggleDrawer(anchor, true)}
+                                >
+                                  {list(anchor)}
+                                </SwipeableDrawer>
+                              </React.Fragment>
+                            ))}
+                          </div>
+                        </div>
                         <p style = {{lineHeight:"2rem"}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, ratione quo! Quibusdam, dolor. Repellat labore iste tempora necessitatibus, tempore at. Iure, excepturi? Tempore dolor eaque cupiditate, eveniet quisquam officiis, assumenda quam, sapiente nisi praesentium aliquid deserunt totam nulla numquam! Nobis distinctio repellat ab velit quam optio expedita voluptatum reiciendis necessitatibus quibusdam perspiciatis in maxime quaerat explicabo, nisi nihil cumque enim, amet deleniti iusto numquam ex debitis eius! Voluptate molestiae iste inventore numquam libero asperiores dolorum consectetur minima, eum et totam, quaerat earum facere. Vero enim harum qui, ex dolorem veritatis, placeat perferendis consequatur dolor optio assumenda. Dicta expedita omnis iusto beatae ullam reprehenderit, quam numquam tenetur quos excepturi quidem et rem harum natus optio. Maiores rem tempora quis iure, recusandae quos accusantium facere incidunt est esse officiis natus sed molestiae quibusdam in, quam, adipisci soluta dolore non voluptates totam vel? Vero corporis libero modi adipisci, dicta sunt distinctio obcaecati expedita!
                         </p>
                     </div>
                     <div>
-                    <h1 className="Connect_title">Connect With Me</h1>
-                    <div className="social_media_bar">
-                        <LinkedInIcon sx = {{fontSize:"30px", color:"#0072b1", cursor:"pointer"}}/>
-                        <InstagramIcon sx = {{fontSize:"30px", color:"#bc2a8d", cursor:"pointer"}} className ="instagram"/>
-                        <FacebookIcon sx = {{fontSize:"30px", color:"#4267B2", cursor:"pointer"}}/>
-                        <TwitterIcon sx = {{fontSize:"30px", color:"#00acee", cursor:"pointer"}}/>
-                    </div>
+                      <h1 className="Connect_title">Connect With Me</h1>
+                      <div className="social_media_bar">
+                          <LinkedInIcon sx = {{fontSize:"30px", color:"#0072b1", cursor:"pointer"}}/>
+                          <InstagramIcon sx = {{fontSize:"30px", color:"#bc2a8d", cursor:"pointer"}} className ="instagram"/>
+                          <FacebookIcon sx = {{fontSize:"30px", color:"#4267B2", cursor:"pointer"}}/>
+                          <TwitterIcon sx = {{fontSize:"30px", color:"#00acee", cursor:"pointer"}}/>
+                      </div>
                     </div>
                 </div>
             </div>
