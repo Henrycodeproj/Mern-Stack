a8854657a71317b8a8d700a0335657b715be1fba
Henry <lihenryhl@yahoo.com>
2023-01-13 00:01:40 -0800

    testing

--- client/src/components/AuthViews/DisplayPage/Test.jsx
+++ client/src/components/AuthViews/DisplayPage/Test.jsx
@@ -33,7 +33,7 @@ function Test() {
         eventClick = {(e)=> p(e)}
         editable = {false}
         headerToolbar = {header}
-        height = "500px"
+        height = "400px"
         dayMaxEvents = {true}
         eventMaxStack = {true}
       />
